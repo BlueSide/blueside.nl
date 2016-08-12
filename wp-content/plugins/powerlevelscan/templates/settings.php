9ffe0af464ad602409f7574d21803bba528ebf94
cikzh <marlonp33@gmail.com>
2016-08-12 17:15:15 +0200

    changed title

--- wp-content/plugins/powerlevelscan/templates/settings.php
+++ wp-content/plugins/powerlevelscan/templates/settings.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-    <h2>WP Plugin Template</h2>
+    <h2>Power Level Scan</h2>
     <form method="post" action="options.php"> 
         <?php @settings_fields('power_level_scan-group'); ?>
         <?php @do_settings_fields('power_level_scan-group'); ?>
