314c3f69dc53f5094ada19b4635b8300d5966abb
cikzh <marlonp33@gmail.com>
2016-08-12 16:47:01 +0200

    implemented boolean icons

--- wp-content/plugins/powerlevelscan/powerlevelscan.php
+++ wp-content/plugins/powerlevelscan/powerlevelscan.php
@@ -1,274 +1,67 @@
 <?php
 /*
-  Plugin Name: Power Level Scan
-  Plugin URI:  http://www.blueside.nl/
-  Description: Enter a URL and dynamically generate a test report for the given website
-  Version:     0.1
-  Author:      Marlon Peeters
-  Author URI:  http://www.blueside.nl/
-  License:     
-  License URI: 
+Plugin Name: Power Level Scan
+Plugin URI: http://www.blueside.nl
+Description: A simple wordpress plugin template
+Version: 0.1
+Author: Marlon Peeters
+Author URI: http://www.blueside.nl
 */
 
-
-/** 
- * NOTE: 
- * - Find a good method for including our JS piece. Checkout wp_enqueue_script
- * - checkout wp_die() for error messages
- * 
- * 
- * 
- * 
- * 
- **/				     
-
-/**
- * Script to set the width of all bs-progress-bar elements
- * 
- * var amountOfElements = document.getElementsByClassName("bs-progress-bar").length;
- * for(var i = 0; i < amountOfElements; i++)
- * {
- * 	document.getElementsByClassName("bs-progress-bar")[i].setAttribute("style", "width: " + document.getElementsByClassName("bs-progress-bar")[i].innerHTML + "%");
- * }
- **/
-
-include_once("debukzh.php");
-require_once("pls_Scan.php");
-
-define('PLS_DATA_TABLE', 'bs_powerlevelscan');
-define('PLS_FIELDS_TABLE', 'bs_powerlevelscan_fields');
-
-
-$scans = new Scans();
-
-function pls_handler($attr)
+if(!class_exists('PLS_Main'))
 {
-    global $wpdb;
-    $output;
-  
-    // Get data
-    $result = $wpdb->get_row(
-        'SELECT `'.$attr['type'].'` FROM `'.PLS_DATA_TABLE.'` WHERE `id` = '.get_the_ID(),
-        ARRAY_A
-                             );
-    
-    // Get representation type
-    switch($attr['out'])
-    {
-        case 'text':
-            $output = $result[$attr['type']];
-            break;
-        case 'boolean':
-            $output = pls_renderBoolean($result[$attr['type']]);
-            break;
-        case 'wheel':
-            break;
-        case 'progressbar':
-            $output = pls_renderProgressbar($result[$attr['type']]);
-            break;
-        default:
-            $output = $result[$attr['type']];
-    }
-    
-    return $output;
-}
-add_shortcode('bs_pls', 'pls_handler');
-
-function pls_renderBoolean($value)
-{   
-    return $value ? get_option('icon_true') : get_option('icon_false');
-}
-
-function pls_renderProgressbar($value)
-{
-    // TODO: add JS script to set the width and color
-    return str_replace('#VALUE#', $value, get_option('progressbar'));
-}
-// Initialze
-add_action("admin_menu", "pls_admin_add_page");
-
-function pls_admin_add_page()
-{
-    add_menu_page("Power Level Scan", "Power Level Scan", "manage_options", "pls_settings", "pls_settings_page");
-    add_action("admin_init", "register_global_pls_settings");
-}
-
-function register_global_pls_settings()
-{
-    //register our settings
-    register_setting( 'pls_global_settings', 'icon_true' );
-    register_setting( 'pls_global_settings', 'icon_false' );
-    register_setting( 'pls_global_settings', 'progressbar' );
-}
-
-function pls_settings_page()
-{
-?>
-    <div class="wrap">
-	<?php
-	global $scans;
-	//TODO: come up with a better check
-    if(isset($_POST['title']))
+	class PLS_Main
 	{
-	    pls_insertPost();
+		/**
+		 * Construct the plugin object
+		 */
+		public function __construct()
+		{
+			// Initialize Settings
+			require_once(sprintf("%s/settings.php", dirname(__FILE__)));
+			$PLS_Settings = new PLS_Settings();
+            
+			// Register custom post types
+			require_once(sprintf("%s/post-types/post_type_template.php", dirname(__FILE__)));
+			$Post_Type_Template = new Post_Type_Template();
+
+			$plugin = plugin_basename(__FILE__);
+            add_filter("plugin_action_links_$plugin", array( $this, 'plugin_settings_link' ));
+		}
+
+		/**
+		 * Activate the plugin
+		 */
+		public static function activate()
+		{
+			// Do nothing
+		} // END public static function activate
+
+		/**
+		 * Deactivate the plugin
+		 */
+		public static function deactivate()
+		{
+			// Do nothing
+		} // END public static function deactivate
+
+		// Add the settings link to the plugins page
+		function plugin_settings_link($links)
+		{
+			$settings_link = '<a href="options-general.php?page=wp_plugin_template">Settings</a>';
+			array_unshift($links, $settings_link);
+			return $links;
+		}
 	}
-	if(isset($_POST['scan_element_name']) && isset($_POST['scan_element_format']))
-	{
-	    $scans->addField($_POST['scan_element_name'], $_POST['scan_element_format']);
-	    $scans->getFields();
-	}
-	?>
-	<div style="margin-top: 30px">
-	    <h1>Create new Power Level Scan</h1>
-	    <h2>Create Scan</h2>
-	    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'].'?page=pls_settings'); ?>" >
-		<table class="form-table">
-		    <tr valign="top">
-			<th scope="row">Site title</th>
-			<td><input type="text" name="title" size="60" /></td>
-		    </tr>
-		    
-		    <tr valign="top">
-			<th scope="row">URL</th>
-			<td><input type="text" name="url" size="60" /></td>
-		    </tr>
-		    
-		    <?php
-		    foreach($scans->fields as $field)
-		    {
-			// Map format to input type
-			$input_type;
-			switch($field['format'])
-			{
-			    case '%d':
-			    case '$f':
-			    case '%s':
-			    $input_type = 'text';
-			    break;
-
-			    case '%b':
-			    $input_type = 'checkbox';
-			    break;
-			}
-			echo '<tr valign="top">';
-			//TODO: Make prettier names
-			echo '<th scope="row">'.$field['name'].'</th>';
-			echo '<td><input type="'.$input_type.'" name="'.$field['name'].'"</td>';
-			echo '</tr>';
-		    }
-		    ?>
-		</table>
-		<input class="button button-primary" name="Submit" type="submit" value="Create Scan" />
-	    </form>
-	    <h2>Add Scan method</h2>
-	    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]."?page=pls_settings"); ?>" >
-		<p>Name<input type="text" name="scan_element_name" /></p>
-		<p>Format<select name="scan_element_format">
-		    <option value="%s">Text</option>
-		    <option value="%d">Number</option>
-		    <option value="%b">Boolean</option>
-		    <option value="%f">Decimal</option>
-		</select></p>
-		<input class="button button-primary" name="Submit" type="submit" value="Add field" />
-	    </form>
-	</div>
-	<h1>Global Power Level Scan settings</h1>
-
-	<form method="post" action="options.php">
-	    <?php settings_fields( 'pls_global_settings' ); ?>
-	    <?php do_settings_sections( 'pls_global_settings' ); ?>
-	    <table class="form-table">
-		<tr valign="top">
-		    <th scope="row">Icon true</th>
-		    <td><input type="text" name="icon_true" value="<?php echo esc_attr( get_option('icon_true') ); ?>" size="60" /></td>
-		</tr>
-		
-		<tr valign="top">
-		    <th scope="row">Icon false</th>
-		    <td><input type="text" name="icon_false" value="<?php echo esc_attr( get_option('icon_false') ); ?>" size="60" /></td>
-		</tr>
-		
-		<tr valign="top">
-		    <th scope="row">Progressbar</th>
-		    <td><input type="text" name="progressbar" value="<?php echo esc_attr( get_option('progressbar') ); ?>" size="60" /></td>
-		</tr>
-	    </table>
-	    
-	    <?php submit_button(); ?>
-
-	</form>
-    </div>
-<?php
 }
 
-function pls_insertPost()
+if(class_exists('PLS_Main'))
 {
-    global $wpdb;
-    global $scans;
-    
-    $title = $_POST['title'];
-    
-    $postData = array(
-        'ID' => $postID,
-        'post_author' => $author,
-        'post_content' => '',
-        'post_content_filtered' => '',
-        'post_title' => $title,
-        'post_excerpt' => '',
-        'post_status' => 'draft',
-        'post_type' => 'page',
-        'comment_status' => '',
-        'ping_status' => '',
-        'post_password' => '',
-        'to_ping' =>  '',
-        'pinged' => '',
-        'post_parent' => 0,
-        'menu_order' => 0,
-        'guid' => '',
-        'import_id' => 0,
-        'context' => '',
-                      );
+	// Installation and uninstallation hooks
+	register_activation_hook(__FILE__, array('PLS_Main', 'activate'));
+	register_deactivation_hook(__FILE__, array('PLS_Main', 'deactivate'));
 
-    $postID = wp_insert_post($postData);
-    
-    if($postID != 0)
-    {
-        $data = ['id' => $postID, 'title' => $title];
-        $format = ['%d', '%s'];
-        
-        foreach($scans->fields as $field)
-        {
-            $data[$field['name']] = pls_getData($field['name'], $field['format']);
-            $format[] = $field['format'];
-        }
-        
-        var_dump($data);
-        // Create new row in bs_powerlevelscan
-        if(!$wpdb->insert(PLS_DATA_TABLE, $data, $format))
-        {
-            echo $wpdb->last_error;
-        }	
-    }
-    else
-    {
-        echo "There was an error creating the post.";
-    }
-    
-}
+	// instantiate the plugin class
+	$wp_plugin_template = new PLS_Main();
 
-function pls_getData($field_name, $format)
-{
-    $result;
-    if($format == '%b')
-    {
-        $result = isset($_POST[$field_name]);
-    }
-    else
-    {
-        $result = $_POST[$field_name];
-    }
-    
-    return $result;
 }
-?>
-</div>
--- wp-content/plugins/powerlevelscan/templates/graphic_types.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$ICON_TRUE = '<i class="uk-icon-check"></i>';
-$ICON_FALSE = '<i class="uk-icon-close"></i>';
-
-$PROGRESS_BAR = '<shit>#VALUE#</shit>';
-
-?>
